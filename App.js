5982bd3de42bdba9b281df094a144246a521deba
Cyrill <cyrillanwar.norazman@rwgenting.com>
2024-02-15 10:06:38 +0800

    Code cleaning

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React, { useContext, useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigation from './src/navigations/TabNavigation'
 import * as Location from 'expo-location';
-import { CurrentLocation } from './src/contexts/CurrentUserLocation';
 import rootStore from './src/store';
 import { Provider } from 'react-redux';
 
@@ -30,11 +29,9 @@ export default function App() {
 
   return (
     <Provider store={rootStore}>
-      {/* <CurrentLocation.Provider value={{ location, setLocation }}> */}
       <NavigationContainer>
         <TabNavigation/>
       </NavigationContainer>
-      {/* </CurrentLocation.Provider> */}
     </Provider>
   )
 }
